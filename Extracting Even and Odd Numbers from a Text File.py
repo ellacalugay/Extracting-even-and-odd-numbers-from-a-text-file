659a891f628393e6a9145c1174e6141582864690
Ella <ellacalugay@gmail.com>
2023-04-23 16:45:08 +0800

    Problem 1 - Details.

---
+++ Extracting Even and Odd Numbers from a Text File.py
@@ -0,0 +1,2 @@
+# Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #4 | PROBLEM 1
+# Creating a program that reads a txt file then extract the even and odd numbers then place it in a two additional text files: "even.txt" with even numbers, and "odd.txt" with odd numbers.
