34e24715bf76ebca67f7edf26adc863029f86199
Ella <ellacalugay@gmail.com>
2023-04-23 16:55:24 +0800

    Read the contents of the file into a list of strings

--- Extracting Even and Odd Numbers from a Text File.py
+++ Extracting Even and Odd Numbers from a Text File.py
@@ -4,7 +4,9 @@
 #Pseudocode
 # Open the file "numbers.txt" for reading.
 with open ("numbers.txt", "r") as numbers:
+    
 # Read the contents of the file into a list of strings.
+    all_numbers = numbers.readlines()
 # Create two new empty lists to store even and odd numbers.
 # Loop through the file "numbers.txt" and check each number.
     # Convert the string to an integer.
