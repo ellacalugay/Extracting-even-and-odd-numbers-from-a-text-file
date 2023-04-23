619ee99ccbe95214bb218c9a974c23cde2f0de86
Ella <ellacalugay@gmail.com>
2023-04-23 16:48:26 +0800

    Pseudocode

--- Extracting Even and Odd Numbers from a Text File.py
+++ Extracting Even and Odd Numbers from a Text File.py
@@ -1,2 +1,13 @@
 # Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #4 | PROBLEM 1
 # Creating a program that reads a txt file then extract the even and odd numbers then place it in a two additional text files: "even.txt" with even numbers, and "odd.txt" with odd numbers.
+
+#Pseudocode
+# Open the file "numbers.txt" for reading.
+# Read the contents of the file into a list of strings.
+# Create two new empty lists to store even and odd numbers.
+# Loop through the file "numbers.txt" and check each number.
+    # Convert the string to an integer.
+    # Check if the number is even or odd and add it to the corresponding list.
+# If the number is even, add it to the list of even numbers (even.txt).
+# If the number is odd, add it to the list of odd numbers (odd.txt).
+# End of the code.
